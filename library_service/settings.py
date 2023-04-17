3c27bb2e7213f0a8c4b5e1ba2952aae63d32a97d
Taras Martyniuk <bythewaters800@gmail.com>
2023-04-17 20:39:39 +0200

    Added drf-spectacular in settings rest-framework

--- library_service/settings.py
+++ library_service/settings.py
@@ -131,6 +131,10 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
 SPECTACULAR_SETTINGS = {
     "TITLE": "Cinema Service API",
     "DESCRIPTION": "Order cinema tickets",
