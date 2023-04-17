38292fb45ab1daf9695fa2a98aea1f5e91f1dcd8
Taras Martyniuk <bythewaters800@gmail.com>
2023-04-17 21:03:37 +0200

    Added urls.py for books app & register books in admin panel

--- library_service/urls.py
+++ library_service/urls.py
@@ -24,6 +24,7 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/library/", include("books.urls", namespace="books")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
