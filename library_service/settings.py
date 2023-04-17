2771ff38fd12a5c3dfbafda3cc7a0abc037f1b9e
Taras Martyniuk <bythewaters800@gmail.com>
2023-04-17 20:17:01 +0200

    Install django-debug-toolbar

--- library_service/settings.py
+++ library_service/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
