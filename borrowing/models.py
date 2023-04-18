101eaf99d4efe98c773bff78b0142d4e9d8e5f73
Taras Martyniuk <bythewaters800@gmail.com>
2023-04-18 16:11:19 +0200

    Added model for borrowing app

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+from books.models import Book
+from library_service import settings
+
 
 class Borrowing(models.Model):
-    pass
+    borrow_date = models.DateTimeField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    books = models.ForeignKey(Book, on_delete=models.CASCADE)
+    users = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+        )
