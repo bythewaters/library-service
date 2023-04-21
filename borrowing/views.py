8efb1625e95562e035f7cfdddb24aebdcdda82d5
Taras Martyniuk <bythewaters800@gmail.com>
2023-04-21 21:35:05 +0200

    Added filtering in borrowing & write documentation

--- borrowing/views.py
+++ borrowing/views.py
@@ -7,7 +7,8 @@ from rest_framework.serializers import Serializer
 from borrowing.models import Borrowing
 from borrowing.serializers import (
     BorrowingSerializer,
-    BorrowingDetailSerializer, BorrowingCreateSerializer
+    BorrowingDetailSerializer,
+    BorrowingCreateSerializer,
 )
 
 
@@ -15,15 +16,35 @@ class BorrowingViewSet(
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
     mixins.CreateModelMixin,
-    viewsets.GenericViewSet
+    viewsets.GenericViewSet,
 ):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
     def get_queryset(self) -> QuerySet[Borrowing]:
-        """Return borrowings only for current user"""
-        return Borrowing.objects.filter(users=self.request.user)
+        """
+        Return borrowings only for current user.
+        Filtering if borrowing has not returned yet.
+        Admin can see all users borrowings, and filtering borrowing by user id.
+        """
+        is_active = self.request.query_params.get("is_active")
+        user_id = self.request.query_params.get("user_id")
+
+        queryset = self.queryset
+
+        if not self.request.user.is_staff:
+            queryset = Borrowing.objects.filter(users=self.request.user)
+
+        if self.request.user.is_staff:
+            if user_id:
+                queryset = queryset.filter(users_id=user_id)
+
+        if is_active:
+            if is_active.lower() == "true":
+                queryset = queryset.filter(actual_return_date=None)
+
+        return queryset
 
     def get_serializer_class(self) -> Type[Serializer]:
         """Return serializer depending on the action"""
