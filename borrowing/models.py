66b7c93a61ed0364a3963337fd8e9bd8de124092
Taras Martyniuk <bythewaters800@gmail.com>
2023-04-21 19:52:46 +0200

    feature: added validators for borrow date and return date

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,13 +1,15 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from books.models import Book
 from library_service import settings
+from django.utils.translation import gettext_lazy as _
 
 
 class Borrowing(models.Model):
     borrow_date = models.DateTimeField()
-    expected_return_date = models.DateField()
-    actual_return_date = models.DateField(null=True, blank=True)
+    expected_return_date = models.DateTimeField()
+    actual_return_date = models.DateTimeField(null=True, blank=True)
     books = models.ForeignKey(Book, on_delete=models.CASCADE)
     users = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
@@ -17,3 +19,11 @@ class Borrowing(models.Model):
             "expected_return_date",
             "actual_return_date",
         )
+
+    def clean(self) -> None:
+        if self.borrow_date > self.expected_return_date:
+            raise ValidationError(_("Expected return date must be after borrow date."))
+
+    def save(self, *args, **kwargs) -> None:
+        self.clean()
+        super().save(*args, **kwargs)
