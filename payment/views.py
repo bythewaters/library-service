ad17d58e8cb05627aabb8c05ab89ee6d0eae43a8
Taras Martyniuk <bythewaters800@gmail.com>
2023-04-26 15:52:23 +0200

    Added permission in PaymentViewSet & get_queryset method

--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,7 @@
 from typing import Type
 
-from rest_framework import viewsets, mixins
+from django.db.models import QuerySet
+from rest_framework import viewsets, mixins, permissions
 from rest_framework.serializers import Serializer
 
 from payment.models import Payment
@@ -11,8 +12,18 @@ class PaymentViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     model = Payment
     queryset = Payment.objects.all()
     serializer_class = PaymentListSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
     def get_serializer_class(self) -> Type[Serializer]:
         """Return serializer depending on the action"""
         if self.action == "retrieve":
             return PaymentDetailSerializer
+
+    def get_queryset(self) -> QuerySet:
+        """
+        Return queryset all payment if user is admin,
+        or filter payments by user.
+        """
+        if not self.request.user.is_staff:
+            queryset = Payment.objects.filter(users=self.request.user)
+        return self.queryset
