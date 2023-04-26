0a385f08ad01f37d3b56f157ce8204eff6e5be98
Taras Martyniuk <bythewaters800@gmail.com>
2023-04-26 15:33:15 +0200

    Added PaymentList & PaymentDetail Serializers

---
+++ payment/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from borrowing.serializers import BorrowingDetailSerializer
+from payment.models import Payment
+
+
+class PaymentListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = [
+            "status",
+            "type",
+            "borrowing",
+            "session_url",
+            "session_id",
+            "money_to_pay",
+        ]
+
+
+class PaymentDetailSerializer(PaymentListSerializer):
+    borrowing = BorrowingDetailSerializer(read_only=True)
