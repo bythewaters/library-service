175fe9f627730670cd866952d9f4f27f34f7ef5e
Taras Martyniuk <bythewaters800@gmail.com>
2023-05-05 22:40:04 +0200

    Change: max_digits in daily_fee Book model

--- books/models.py
+++ books/models.py
@@ -10,7 +10,7 @@ class Book(models.Model):
     author = models.CharField(max_length=255)
     cover = models.CharField(max_length=63, choices=Cover.choices)
     inventory = models.PositiveIntegerField()
-    daily_fee = models.DecimalField(max_digits=2, decimal_places=2)
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self) -> str:
         return self.title
