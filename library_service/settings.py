aaf55edf8d709c1055c38031ed961a350a7cc7f2
Taras Martyniuk <bythewaters800@gmail.com>
2023-05-06 16:58:20 +0200

    Connect to postgresql database

--- library_service/settings.py
+++ library_service/settings.py
@@ -93,8 +93,11 @@ WSGI_APPLICATION = "library_service.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
     }
 }
 
