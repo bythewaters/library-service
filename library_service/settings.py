cc58f845e81ee29130f286f042ee0db566b945aa
Taras Martyniuk <bythewaters800@gmail.com>
2023-05-06 16:12:36 +0200

    Fix library_service_data and change USE_TZ

--- library_service/settings.py
+++ library_service/settings.py
@@ -131,7 +131,7 @@ TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
